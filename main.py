fc44bbf30c55211101589dbd3a1f9be3a7e1132e
Rhuan <rhuan.lima@gmail.com>
2023-11-10 01:18:33 -0300

    add account sistem crud

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
-from model.db_crud import Base, Account, Category, Subcategory, Transaction
+from model.db_crud import Base, Account, Category, Transaction
 from model.log import get_log
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 logger = get_log()
 
 app = Flask(__name__)
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 engine = create_engine("sqlite:///data/pynance.sqlite")
 Base.metadata.create_all(engine)
 
@@ -19,10 +19,76 @@ logger.info("Iniciando o aplicativo")
 @app.route("/")
 def index():
     session = Session()
-    accounts = session.query(Account).all()
-    categories = session.query(Category).all()
+
+    # Obtém o total do saldo nas contas ativas considerando todas as transações, agrupado por conta
+    total_balances = (
+        session.query(
+            Account.des_account,
+            func.sum(Transaction.vl_transaction).label("total_balance"),
+        )
+        .join(Transaction, Transaction.id_account == Account.id)
+        .filter(Account.fl_active == True)
+        .group_by(Account.id)
+        .all()
+    )
+
     session.close()
-    return render_template("index.html", accounts=accounts, categories=categories)
+
+    return render_template("index.html", total_balances=total_balances)
+
+
+@app.route("/accounts")
+def pg_accounts():
+    session = Session()
+    accounts = session.query(Account).all()
+    session.close()
+    return render_template("accounts.html", accounts=accounts)
+
+
+@app.route("/edit_account/<int:id>", methods=["GET", "POST"])
+def edit_account(id):
+    session = Session()
+    account = session.query(Account).get(id)
+
+    if request.method == "POST":
+        # Atualiza o nome da conta com base no formulário enviado
+        new_account_name = request.form.get("new_account_name")
+        account.des_account = new_account_name
+        session.commit()
+        session.close()
+        flash("Conta alterada com sucesso!")
+        return redirect(url_for("pg_accounts"))
+
+    session.close()
+    return render_template("edit_account.html", account=account)
+
+
+@app.route("/deactivate_account/<int:id>")
+def deactivate_account(id):
+    session = Session()
+    account = session.query(Account).get(id)
+
+    # Desativa a conta
+    account.fl_active = False
+    account_name = account.des_account
+    session.commit()
+    session.close()
+    flash(f"Conta {account_name} foi desativada!")
+    return redirect(url_for("pg_accounts"))
+
+
+@app.route("/activate_account/<int:id>")
+def activate_account(id):
+    session = Session()
+    account = session.query(Account).get(id)
+
+    # Ativa a conta
+    account.fl_active = True
+    account_name = account.des_account
+    session.commit()
+    session.close()
+    flash(f"Conta {account_name} foi ativada!")
+    return redirect(url_for("pg_accounts"))
 
 
 @app.route("/create_account", methods=["POST"])
@@ -33,7 +99,15 @@ def create_account():
     session.add(new_account)
     session.commit()
     session.close()
-    return redirect(url_for("index"))
+    return redirect(url_for("pg_accounts"))
+
+
+@app.route("/categorias")
+def pg_categories():
+    session = Session()
+    categories = session.query(Category).all()
+    session.close()
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/create_category", methods=["POST"])
--- model/log.py
+++ model/log.py
@@ -9,14 +9,22 @@ def get_log():
             "f": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"}
         },
         handlers={
-            "h": {
+            "file": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "formatter": "f",
+                "level": logging.DEBUG,
+                "filename": "app.log",
+                "maxBytes": 1048576,  # 1 MB
+                "backupCount": 3,  # Manter até 3 arquivos de backup
+            },
+            "stream": {
                 "class": "logging.StreamHandler",
                 "formatter": "f",
                 "level": logging.DEBUG,
-            }
+            },
         },
         root={
-            "handlers": ["h"],
+            "handlers": ["file", "stream"],
             "level": logging.DEBUG,
         },
     )
